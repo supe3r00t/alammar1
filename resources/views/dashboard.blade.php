6bd96b991407d09f9173ee3b15a5d49a9a4d2a76
supe3r00t <ksa0.net@email.com>
2023-06-12 07:59:22 +0300

    Edit date

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,46 +3,91 @@
 @section('body')
 
 
-    <table class="table">
-        <thead>
-        <tr>
+    <div class="card text-center">
+        <div class="card-header">
+
+            ملتقى جامعة المجمعة الأول
 
 
+        </div>
+        <div class="card-body">
 
+            <h5 class="card-title"> </h5>
             <div class="input-group mb-3 search-form-wrapper">
-            <form action="{{route('search.index')}}" method="GET" name="Search" title="إبحث في الموقع">
-                <div class="card-header col-auto" id="Search">
-                    <input name="search" type="search" class="form-control">
-                    <button class="search__btn ">البحث</button>
+                <form action="{{route('search.index')}}" method="GET" name="Search" title="إبحث في الموقع">
+                    <div class="card-header col-auto" id="Search">
+                        <input name="search" type="search" class="form-control">
+                        <button class="search__btn ">البحث</button>
+                    </div>
+                </form>
+
+
+                <div class="card" style="width: 10rem;">
+                    <div class="card-body">
+                        <a href="{{route('events.events')}}" class="btn btn-secondary stretched-link">{{ __('IndexEvents') }}</a>
+                    </div>
                 </div>
-            </form>
+
+                <div class="card" style="width: 10rem;">
+                    <div class="card-body">
+                        <a href="{{route('oldindex')}}" class="btn btn-danger stretched-link">{{ __('Oldevents') }}</a>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 10rem;">
+                    <div class="card-body">
+                        <a href="{{route('events.workshop')}}" class="btn btn-secondary stretched-link">{{ __('Workshop') }}</a>
+                    </div>
+                </div>
+
+
+@guest
+                <div class="card" style="width: 10rem;">
+                    <div class="card-body">
+                        <a href="{{route('login')}}" class="btn btn-secondary stretched-link">{{ __('Login') }}</a>
+                    </div>
+                </div>
+                @endguest
+
+
+                @auth
+                    <div class="card" style="width: 10rem;">
+                        <div class="card-body">
+                            <a href="{{route('admin.events.index')}}" class="btn btn-dark stretched-link">{{ __('لوحة التحكم') }}</a>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 10rem;">
+                        <div class="card-body">
+                            <a href="{{route('admin.events.create')}}" class="btn btn-dark stretched-link">{{ __('Create') }}</a>
+                        </div>
+                    </div>
+
+
+                @endauth
+                <p class="card-text"></p>
+
+
+
+
             </div>
 
 
 
             </div>
-            <div class="card" style="width: 10rem;">
-                <div class="card-body">
-                    <a href="{{route('events.events')}}" class="btn btn-secondary stretched-link">{{ __('IndexEvents') }}</a>
-                </div>
-            </div>
-            <div class="card" style="width: 10rem;">
-                <div class="card-body">
-                    <a href="{{route('oldindex')}}" class="btn btn-danger stretched-link">{{ __('Oldevents') }}</a>
-                </div>
-            </div>
+
+
 
             <br>
 
-            <div class="card" style="width: 10rem;">
-                <div class="card-body">
-                    <a href="{{route('events.workshop')}}" class="btn btn-secondary stretched-link">{{ __('Workshop') }}</a>
-                </div>
-            </div>
 
 
 
-    </table>
+
+
+    </div>
+    <div class="card-footer text-body-secondary">
+    </div>
+    </div>
 
 
 
